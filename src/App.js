dbbda8a06274d96fa0124e3e1bc6d01534e92a67
David <aspiration@163.com>
2021-01-06 17:20:07 +0800

    修改App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          欢迎来到我的React Project.
+          欢迎来到我的React Project.This my first repository on GitHub.
         </p>
         <a
           className="App-link"
