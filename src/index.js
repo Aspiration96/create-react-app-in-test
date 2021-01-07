20fd98ed0c653b369c10d6d48bb02163fbc3a413
David <aspiration@163.com>
2021-01-07 18:09:39 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
-import React from 'react';
+// import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HeadNav from './HeadNav';
+import RouterWrap from './router';
 import reportWebVitals from './reportWebVitals';
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
+import './HeadNav.less';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+// ReactDOM.render(
+//   // <React.StrictMode>
+//     <HeadNav />,
+//   // {/* </React.StrictMode>, */}
+//   document.getElementById('root')
+// );
+
+// export default class DefaultLayout extends Component{
+//   render(){
+//       return (
+//           <div id="root">
+//               <HeadNav/>
+//           </div>
+//       )
+//   }
+// }
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <RouterWrap />,
+  // {/* </React.StrictMode>, */}
   document.getElementById('root')
 );
 
